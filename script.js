83e80b3dcfbcba6f2ed86d5805cfeae4c17e3c63
Mrs Sharmin Akter <zamalzamalhossain@gmail.com>
2021-02-08 12:19:28 +0100

    added file

---
+++ script.js
@@ -0,0 +1,30 @@
+const searchmeals = () =>{
+    const searchMeal = document.getElementById('search-meal').value;
+    const url =`https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood`
+
+    fetch (url)
+    .then(res => res.json())
+    .then(data => displayMeals(data.meals))
+    
+}
+const displayMeals = meals => {
+    const foodContainer = document.getElementById('food-container');
+
+    meals.forEach(meal => {
+        console.log(meal);
+        const foodDiv =document.createElement('div');
+        foodDiv.className = 'food p-3';
+        foodDiv.innerHTML =` 
+        <div class ="col-md-6 "> 
+           <a><img src ="${meal.strMealThumb}"></a>
+            <h3 class="food-name">${meal.strMeal}</h3> 
+        </div> 
+        `;
+        foodContainer.appendChild(foodDiv);
+
+    })
+}
+// document.getElementById('search').addEventListener('click', function(){
+//    const input = document.getElementById("search-meal").value;
+//    console.log(input);
+// })
