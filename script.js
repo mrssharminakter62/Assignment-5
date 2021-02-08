ade49a3d1f07b5b586c21de973bb752b1d61a903
Mrs Sharmin Akter <zamalzamalhossain@gmail.com>
2021-02-08 15:52:42 +0100

    added file

--- script.js
+++ script.js
@@ -24,7 +24,37 @@ const displayMeals = meals => {
 
     })
 }
-// document.getElementById('search').addEventListener('click', function(){
-//    const input = document.getElementById("search-meal").value;
-//    console.log(input);
-// })
+// ingredient==========================
+
+
+const displayMealsDetail = (meal) => {
+    const url =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=a`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => mealIngredientInfo(data.meals[1]));
+}
+const mealIngredientInfo = meals => {
+    const foodDetail = document.getElementById('food-detail');
+    meals.forEach(meal =>{
+        console.log(meal);
+        const foodIngre = document.createElement('div');
+       
+        foodIngre.innerHTML= `
+        <img src ="${meal.strMealThumb}">
+        <h3>${meal.strMeal}</h3>
+        <ul>
+            <p>Ingredients</p>
+    
+            <li>${meal.strIngredient1}</li>
+            <li>${meal.strIngredient2}</li>
+            <li>${meal.strIngredient3}</li>
+            <li>${meal.strIngredient4}</li>
+            <li>${meal.strIngredient5}</li>
+            <li>${meal.strIngredient6}</li>
+        </ul>
+    `;
+    foodDetail.appendChild(foodIngre);
+    })
+   
+
+}
